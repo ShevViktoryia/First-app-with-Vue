59666ba0928a9d2bd108d01d8bef5c0dc2cb5079
Viktoryia Shevchenko <shevviktoryia@gmail.com>
2023-10-22 17:17:17 +0300

    feat: add special attack functionality

--- monster-slayer/app.js
+++ monster-slayer/app.js
@@ -2,7 +2,8 @@ Vue.createApp({
   data() {
     return {
       playerHealth: 100,
-      monsterHealth: 100
+      monsterHealth: 100,
+      round: 0
     }
   },
   computed: {
@@ -11,17 +12,27 @@ Vue.createApp({
     },
     playerHealthBar() {
       return {width: this.playerHealth + '%' };
+    },
+    useSpecialAttack() {
+      return this.round % 3;
     }
   },
   methods: {
     attackMonster() {
       const value = Math.floor(Math.random() * (12 - 5)) + 5;
       this.monsterHealth -= value;
+      this.round++;
       this.attackPlayer();
     },
     attackPlayer() {
       const value = Math.floor(Math.random() * (15 - 8)) + 8;
       this.playerHealth -= value;
+    },
+    specialAttack() {
+      const value = Math.floor(Math.random() * (30 - 10)) + 10;
+      this.monsterHealth -= value;
+      this.round++;
+      this.attackPlayer();
     }
   }
 }).mount("#game");
